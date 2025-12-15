571e9e41e233e52e93b5aac8cdc6e8712b6e3016
Nicos Panayides <nicosp@gmail.com>
2025-12-15 13:20:21 +0200

    Check PHP 8.5 ase well. Update phpstan and phpcs and fix errors

--- tests/Http/ResponseTest.php
+++ tests/Http/ResponseTest.php
@@ -10,7 +10,8 @@ class ResponseTest extends TestCase
     #[Test]
     public function itCreatesValidXml(): void
     {
-        $body = "<?xml version='1.0' encoding='UTF-8'?><guestbook><guest><fname>First Name</fname><lname>Last Name</lname></guest></guestbook>";
+        $body = "<?xml version='1.0' encoding='UTF-8'?><guestbook><guest><fname>First Name</fname>" .
+            "<lname>Last Name</lname></guest></guestbook>";
         $guzzleResponse = new \GuzzleHttp\Psr7\Response(200, ['X-Foo' => 'Bar'], $body);
 
         $response = new Response($guzzleResponse);
@@ -21,7 +22,8 @@ class ResponseTest extends TestCase
     #[Test]
     public function itCreatesValidXmlWithNewLines(): void
     {
-        $body = "\n\n\r<?xml version='1.0' encoding='UTF-8'?><guestbook><guest><fname>First Name</fname><lname>Last Name</lname></guest></guestbook>\r\n\n";
+        $body = "\n\n\r<?xml version='1.0' encoding='UTF-8'?><guestbook><guest><fname>First Name</fname>" .
+            "<lname>Last Name</lname></guest></guestbook>\r\n\n";
         $guzzleResponse = new \GuzzleHttp\Psr7\Response(200, ['X-Foo' => 'Bar'], $body);
 
         $response = new Response($guzzleResponse);
